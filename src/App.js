9a2e947484cd30c779c3384140c657e440457805
fnitsch <f.nitsch@gmail.com>
2021-02-18 14:29:47 -0500

    Adding Global styles and About Us styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "./Components/GlobalStyle";
 //Import Pages
 import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
