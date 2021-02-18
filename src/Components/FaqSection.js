63b7d57a42b5fbcbe80cd4c837400df6376dd7c2
fnitsch <f.nitsch@gmail.com>
2021-02-18 12:03:30 -0500

    Basic about us page info complete

---
+++ src/Components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            nulla!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What is your daily schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            nulla!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What payment methods do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            nulla!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            nulla!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
