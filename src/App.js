8e75b17ed648c541c3180573925abb154a74eda5
fnitsch <f.nitsch@gmail.com>
2021-02-18 16:18:56 -0500

    More styling, router, and page routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from "react";
 import GlobalStyle from "./Components/GlobalStyle";
 //Import Pages
 import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import OurWork from "./Pages/OurWork";
+import Nav from "./Components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
