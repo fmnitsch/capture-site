d1c0c7cddc9b954d671d6d74e25c8f3e7901a541
fnitsch <f.nitsch@gmail.com>
2021-02-19 14:47:55 -0500

    More styling, baisc page transitions

--- src/App.js
+++ src/App.js
@@ -6,25 +6,34 @@ import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
 import OurWork from "./Pages/OurWork";
 import Nav from "./Components/Nav";
+import MovieDetail from "./Pages/MovieDetail";
 //Router
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work">
-          <OurWork />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,14 +3,22 @@ import React from "react";
 import AboutSection from "../Components/AboutSection";
 import ServicesSection from "../Components/ServicesSection";
 import FaqSection from "../Components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../Animation";
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
